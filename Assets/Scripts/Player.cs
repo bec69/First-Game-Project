a9721262bc06804149fc302a534adebf8359615b
Dmitry <kubarsky.business@gmail.com>
2021-03-03 22:12:10 +0200

    well that was mind melting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform groundCheckTransform;
+    [SerializeField] private LayerMask playerMask;
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigitbodyComponent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigitbodyComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (true)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            
+            jumpKeyWasPressed = true;
         }
+        horizontalInput = Input.GetAxis("Horizontal");
+
     }
+
+    private void FixedUpdate()
+    {
+            rigitbodyComponent.velocity = new Vector3(horizontalInput, rigitbodyComponent.velocity.y, 0);
+
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }
+    
+        if (jumpKeyWasPressed == true)
+        {
+            rigitbodyComponent.AddForce(Vector3.up * 7, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+    }
+    
+     
 }
